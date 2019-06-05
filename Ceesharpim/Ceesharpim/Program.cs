3578c0702deba18626931ba227ea717f79de03f4
Tim <parshintn@gmail.com>
2019-06-05 22:30:16 +0700

    Created solution

---
+++ Ceesharpim/Ceesharpim/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ceesharpim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hey!");
+            Console.WriteLine("What you're looking for watch tonight?");
+            string answer = Console.ReadLine();
+            Console.WriteLine("Ok! It's a new " + answer + " for your. Enjoy it!");
+        }
+    }
+}
